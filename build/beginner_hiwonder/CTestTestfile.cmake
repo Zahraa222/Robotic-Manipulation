18fb5d68bd03882edc29e76b0b7fc8c3ea827a57
Zahraa222 <zahraaaa@umich.edu>
2025-04-24 20:29:06 -0400

    clear repo to avoid merge conflicts

--- build/beginner_hiwonder/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zahraa/catkin_ws/src/beginner_hiwonder
-# Build directory: /home/zahraa/catkin_ws/build/beginner_hiwonder
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
